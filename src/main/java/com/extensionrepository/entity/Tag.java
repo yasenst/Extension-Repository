d243ba71368392606e212677d36bd47707ac635d
PenyoKolev <penyokolev@gmail.com>
2018-08-22 16:43:13 +0300

    Add Tag entity

---
+++ src/main/java/com/extensionrepository/entity/Tag.java
@@ -0,0 +1,58 @@
+package com.extensionrepository.entity;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+public class Tag {
+
+    @Id
+    @Column(name = "tag_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "extensions_tags",
+    joinColumns = @JoinColumn(name = "tag_id"),
+    inverseJoinColumns = @JoinColumn(name = "extension_id"))
+    private Set<Extension> extensions;
+
+    public Tag(){
+        this.extensions = new HashSet<>();
+    }
+
+    public Tag(String name, Set<Extension> extensions) {
+        this.name = name;
+
+        this.extensions = new HashSet<>();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Set<Extension> getExtensions() {
+        return extensions;
+    }
+
+    public void setExtensions(Set<Extension> extensions) {
+        this.extensions = extensions;
+    }
+}
