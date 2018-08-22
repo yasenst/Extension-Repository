71570922962657fa82eef9b1479d4ab0282b786f
Yasen <yasen9977@gmail.com>
2018-08-22 18:28:02 +0300

    Add WebUserDetails extends User impl UserDetails in cofiguration

---
+++ src/main/java/com/extensionrepository/configuration/WebUserDetails.java
@@ -0,0 +1,51 @@
+package com.extensionrepository.configuration;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.util.StringUtils;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class WebUserDetails extends User implements UserDetails {
+    private ArrayList<String> roles;
+    private User user;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        String userRoles = StringUtils.collectionToCommaDelimitedString(this.roles);
+        return AuthorityUtils.commaSeparatedStringToAuthorityList(userRoles);
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public WebUserDetails(User user, ArrayList<String> roles) {
+        super(user.getUsername(), user.getFullname(), user.getPassword());
+
+        this.user = user;
+        this.roles = roles;
+    }
+}
