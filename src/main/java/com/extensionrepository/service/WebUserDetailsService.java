27d145c8a03c9f27601ff716fb7a348db72fec36
Yasen <yasen9977@gmail.com>
2018-08-23 01:27:04 +0300

    Add class WebUserDetailsService implementing UserDetailsService

---
+++ src/main/java/com/extensionrepository/service/WebUserDetailsService.java
@@ -0,0 +1,42 @@
+package com.extensionrepository.service;
+
+import com.extensionrepository.entity.User;
+import com.extensionrepository.repositories.UserRepository;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Service("webUserDetailsService")
+public class WebUserDetailsService implements UserDetailsService {
+    private UserRepository userRepository;
+
+    public WebUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username);
+        if (user == null) {
+            throw new UsernameNotFoundException("Invalid user");
+        } else {
+            Set<GrantedAuthority> grantedAuthorities = user.getRoles()
+                    .stream()
+                    .map(role -> new SimpleGrantedAuthority(role.getName()))
+                    .collect(Collectors.toSet());
+
+            return new org
+                    .springframework
+                    .security
+                    .core
+                    .userdetails
+                    .User(user.getUsername(),user.getPassword(),grantedAuthorities);
+        }
+    }
+}
