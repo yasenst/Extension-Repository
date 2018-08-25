fb297aaff46788d9667c64d3c1ed865882477a8b
Yasen <yasen9977@gmail.com>
2018-08-25 22:14:10 +0300

    Add login page

--- src/main/java/com/extensionrepository/controller/mvcController/LoginController.java
+++ src/main/java/com/extensionrepository/controller/mvcController/LoginController.java
@@ -5,11 +5,11 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class HomeController {
+public class LoginController {
 
-    @GetMapping("/")
-    public String index(Model model) {
-        model.addAttribute("view", "home/index");
+    @GetMapping("/login")
+    public String login(Model model) {
+        model.addAttribute("view", "login");
         return "base-layout";
     }
 }
