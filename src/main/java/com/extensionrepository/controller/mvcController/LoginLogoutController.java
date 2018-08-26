67532e4b21f5538d1a58ce54f66cfd04ddc67128
Yasen <yasen9977@gmail.com>
2018-08-27 00:31:25 +0300

    Refactored code structure

--- src/main/java/com/extensionrepository/controller/mvcController/LoginLogoutController.java
+++ src/main/java/com/extensionrepository/controller/mvcController/LoginLogoutController.java
@@ -4,6 +4,8 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -11,9 +13,16 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 @Controller
-public class LogoutController {
+public class LoginLogoutController {
 
-    @RequestMapping(value="/logout", method = RequestMethod.GET)
+    @GetMapping("/login")
+    public String login(Model model) {
+        model.addAttribute("view", "login");
+
+        return "base-layout";
+    }
+
+    @GetMapping("/logout")
     public String logoutPage(HttpServletRequest request, HttpServletResponse response) {
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
 
