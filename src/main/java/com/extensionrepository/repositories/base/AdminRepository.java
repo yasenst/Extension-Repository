66a3974e1ecfc5661be210144d9cb3cfe57714b8
PenyoKolev <penyokolev@gmail.com>
2018-08-27 13:00:32 +0300

    Add AdminRepository

---
+++ src/main/java/com/extensionrepository/repositories/base/AdminRepository.java
@@ -0,0 +1,16 @@
+package com.extensionrepository.repositories.base;
+
+import com.extensionrepository.entity.Extension;
+
+public interface AdminRepository {
+
+    void enableUser(String name);
+
+    void disableUser(String name);
+
+    void approveExtension(int id);
+
+    void editExtension(int id);
+
+    void deleteExtension(Extension extension);
+}
