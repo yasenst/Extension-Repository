19b81f887d8980f474378239a638a08300b0c43c
Yasen <yasen9977@gmail.com>
2018-09-03 03:13:23 +0300

    Add admin option to extension detail, broken feature extension in admin panel

--- src/main/java/com/extensionrepository/controller/mvcController/AdminController.java
+++ src/main/java/com/extensionrepository/controller/mvcController/AdminController.java
@@ -10,10 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder;
 
 import java.util.List;
@@ -122,6 +119,6 @@ public class AdminController {
     @GetMapping("/admin/featured/toggle-status/{id}")
     public String toggleFeaturedStatus(@PathVariable int id) {
         extensionService.changeStatus(id);
-        return "redirect:/admin/featured";
+        return "redirect:/extension/" + id;
     }
 }
