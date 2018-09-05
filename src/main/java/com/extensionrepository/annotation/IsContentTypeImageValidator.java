b0a9df1d202428f743e3166d10e8dd5129ccd4ed
Yasen <yasen9977@gmail.com>
2018-09-05 03:37:31 +0300

    Admin can synchronize extension with github

--- src/main/java/com/extensionrepository/annotation/IsContentTypeImageValidator.java
+++ src/main/java/com/extensionrepository/annotation/IsContentTypeImageValidator.java
@@ -15,6 +15,9 @@ public class IsContentTypeImageValidator implements ConstraintValidator<IsConten
     @Override
     public boolean isValid(Object extensionClass, ConstraintValidatorContext context) {
         if (extensionClass instanceof ExtensionDto) {
+            if (((ExtensionDto) extensionClass).getImage().getOriginalFilename().equals("")) {
+                return true;
+            }
             return ((ExtensionDto) extensionClass)
                     .getImage().getContentType().equals("image/png")
                     || ((ExtensionDto) extensionClass)
